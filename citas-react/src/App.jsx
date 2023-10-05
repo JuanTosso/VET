33a9a1b2728f9943ccc099fc83f1b1d1f32538be
JuanTosso <juantosso89@gmail.com>
2023-10-05 09:56:12 -0300

    creando componentes

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import header from "./components/Headers";
+
+const App = () => {
   return (
     <>
       <h1 className="App">Hello World</h1>
     </>
   );
-}
+};
 
 export default App;
